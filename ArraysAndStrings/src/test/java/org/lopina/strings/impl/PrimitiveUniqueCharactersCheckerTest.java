e944ab8d015495f4bab5b536a5bf14da33b6090a
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2016-04-07 22:22:33 +0200

    - Implemented O(n^2) PrimitiveUniqueCharactersChecker   - Added JUnit tests for PrimitiveUniqueCharactersChecker

---
+++ ArraysAndStrings/src/test/java/org/lopina/strings/impl/PrimitiveUniqueCharactersCheckerTest.java
@@ -0,0 +1,14 @@
+package org.lopina.strings.impl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.lopina.strings.UniqueCharacterCheckerTest;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class PrimitiveUniqueCharactersCheckerTest extends UniqueCharacterCheckerTest {
+
+    @Override
+    protected PrimitiveUniqueCharactersChecker getUniqueCharacterChecker() {
+        return new PrimitiveUniqueCharactersChecker();
+    }
+}
